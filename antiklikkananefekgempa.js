0acebc7279b2faee2f4c7e9e9268e56d61de3342
Mbah Dinan <etnikprogresif@gmail.com>
2016-02-08 00:45:01 -0800

    Update antiklikkananefekgempa.js

--- antiklikkananefekgempa.js
+++ antiklikkananefekgempa.js
@@ -1,5 +1,5 @@
 function shake(n) {if (parent.moveBy){for (i = 10000; i > 0; i--){for (j = n; j > 0; j--){self.resizeTo(150,150);parent.moveBy(i,0);parent.moveBy(0,-i);parent.moveBy(-i,0);parent.moveBy(0,i);parent.moveBy(0,i);}}}}
-PopUpURL = "Mo Ngapain, sih?";isIE=document.all;isNN=!document.all&&document.getElementById;
+PopUpURL = "No right click!!! Use CTRL + C for Copy";isIE=document.all;isNN=!document.all&&document.getElementById;
 isN4=document.layers;if (isIE||isNN){document.oncontextmenu=checkV;}
 else{document.captureEvents(Event.MOUSEDOWN || Event.MOUSEUP);document.onmousedown=checkV;}
 function checkV(e){if (isN4){if (e.which==2||e.which==3){dPUW=alert("No right click, please .....");
